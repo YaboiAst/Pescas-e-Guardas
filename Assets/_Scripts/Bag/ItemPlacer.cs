54c903819c890e8e0aaf29c220d64b2a86a872d2
Ast <93723066+YaboiAst@users.noreply.github.com>
2025-06-10 11:39:03 -0300

    update: Inventory changes for overlapping items

--- Assets/_Scripts/Bag/ItemPlacer.cs
+++ Assets/_Scripts/Bag/ItemPlacer.cs
@@ -2,12 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemPlacer : MonoBehaviour
 {
     [SerializeField] private RectTransform blocksRoot;
     [SerializeField] private List<ItemPlacerBlock> blocksRects;
 
+    private bool _isOnGrid = false;
+    public void SetPositionStatus(bool status) => _isOnGrid = status;
+    public bool GetPositionStatus() => _isOnGrid;
+    
     public RectTransform GetRoot()
     {
         return blocksRoot;
@@ -23,6 +28,25 @@ public class ItemPlacer : MonoBehaviour
         return rectList;
     }
 
+    public void PlaceItem(Image targetVisual, Vector3 newPosition, float newRotation)
+    {
+        transform.position = newPosition;
+        
+        targetVisual.transform.eulerAngles = Vector3.forward * newRotation;
+        GetRoot().transform.eulerAngles = Vector3.forward * newRotation;
+        
+        this.GetRoot().position = transform.position;
+        targetVisual.transform.position = transform.position;
+        
+        // Add Fish Data to inventory
+        InventoryController.ClearGrid?.Invoke();
+    }
+
+    public void SetItemInGrid()
+    {
+        InventoryController.Instance.UpdatePlacement(this);
+    }
+    
     public void SnapToGrid(RectTransform tile)
     {
         var newX = tile.position.x - (tile.rect.width / 2) + (blocksRoot.rect.width / 2);
