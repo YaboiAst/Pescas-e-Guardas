9589ca6866b51c03b146d2dd00611a5e918b93c7
Pedronsa Dev <84478967+PedronsaDev@users.noreply.github.com>
2025-06-28 05:11:54 -0300

    Add world state

--- Assets/_Scripts/Water/WaterInteraction.cs
+++ Assets/_Scripts/Water/WaterInteraction.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class WaterInteraction : MonoBehaviour
 {
-    [SerializeField] RenderTexture renderTexture;
-    [SerializeField] Transform interactor;
+    [SerializeField] RenderTexture _renderTexture;
+    [SerializeField] Transform _interactor;
     [SerializeField] Material _waterMaterial;
 
     void Awake()
     {
-        _waterMaterial.SetTexture("_GlobalEffectRT", renderTexture);
+        _waterMaterial.SetTexture("_GlobalEffectRT", _renderTexture);
         _waterMaterial.SetFloat("_ProjectionSize", GetComponent<Camera>().orthographicSize);
-        //Shader.SetGlobalTexture("_GlobalEffectRT", renderTexture);
-        //Shader.SetGlobalFloat("_ProjectionSize", GetComponent<Camera>().orthographicSize);
     }
 
     private void Update()
     {
-        transform.position = new Vector3(interactor.transform.position.x, transform.position.y, interactor.transform.position.z);
-        //Shader.SetGlobalVector("_Position", transform.position);
+        transform.position = new Vector3(_interactor.transform.position.x, transform.position.y, _interactor.transform.position.z);
         _waterMaterial.SetVector("_Position", transform.position);
     }
 
