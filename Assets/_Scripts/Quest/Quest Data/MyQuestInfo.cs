8bebafc61b5c4cbc79b4cd46ffb7c986b97fc428
PontuuuCOM <pedokstionacho@gmail.com>
2025-06-29 19:52:33 -0300

    Algo do tutorial

--- Assets/_Scripts/Quest/Quest Data/MyQuestInfo.cs
+++ Assets/_Scripts/Quest/Quest Data/MyQuestInfo.cs
@@ -6,6 +6,7 @@ public class MyQuestInfo
     public string questGiver;
     public string title;
     public int pointReward;
+    public int points;
 
     [TextArea] public string description;
     public string objectiveDescription;
